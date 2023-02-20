aa2ecc992aa12056a338d42f2d4a3078c55e7bc1
maxefimov <efimov.academy.top@gmail.com>
2023-02-20 16:23:10 +0300

    Добавьте файлы проекта.

---
+++ FPTypesProject/FPTypesProject.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a;
+
+int main()
+{
+    //int n;
+    //long long m;
+    //cout << ceil(2.1) << "\n";
+    //cout << ceil(2.9) << "\n";
+    //cout << ceil(-2.1) << "\n";
+    //cout << ceil(-2.9) << "\n";
+    //cout << "\n";
+    //cout << floor(2.1) << "\n";
+    //cout << floor(2.9) << "\n";
+    //cout << floor(-2.1) << "\n";
+    //cout << floor(-2.9) << "\n";
+    //cout << "\n";
+    //cout << trunc(2.1) << "\n";
+    //cout << trunc(2.9) << "\n";
+    //cout << trunc(-2.1) << "\n";
+    //cout << trunc(-2.9) << "\n";
+    //cout << "\n";
+    //cout << round(2.1) << "\n";
+    //cout << round(2.9) << "\n";
+    //cout << round(-2.1) << "\n";
+    //cout << round(-2.9) << "\n";
+
+    int n{ 10 };
+    a = 30;
+    
+    {
+        a = 20;
+        cout << a << "\n";
+    }
+
+
+    cout << a << "\n";
+}
